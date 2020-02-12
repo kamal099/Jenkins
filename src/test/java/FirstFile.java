9046e8bed103b051627c49b9a585d8ba1a900aaf
kamal099 <kamal_afg786@hotmail.com>
2020-02-11 21:54:49 -0500

    MyFirst Commit

---
+++ src/test/java/FirstFile.java
@@ -0,0 +1,14 @@
+
+public class FirstFile {
+
+	public static void main(String[] args) {
+
+		int i;
+		for(i=0;i<=10;i++) {
+			
+			System.out.println("This run form maven project");
+		}
+		
+	}
+
+}
